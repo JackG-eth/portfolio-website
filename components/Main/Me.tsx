480c62a83f5e5f66974ddae97261df088a1307e3
JackTracerSpore <jack.hawkins@mycelium.ventures>
2022-08-01 18:48:40 +0100

    Picture Changes (Pontential)

--- components/Main/Me.tsx
+++ components/Main/Me.tsx
@@ -5,10 +5,12 @@ const Me = () => {
 		<div className="relative flex h-screen w-screen flex-col items-center justify-center">
 			<img
 				className="flex h-[300px] w-[300x] rounded-full border-8 shadow-xl"
-				src="/static/img/meBW.jpg"
+				src="/static/img/me.png"
 				alt="Mycelium logo"
 			/>
 			<ReactTypingEffect
+				speed={200}
+				eraseDelay={2500}
 				className="pt-4 text-[32px]  leading-tight text-white"
 				text={[
 					"Web3 Full-Stack Developer",
