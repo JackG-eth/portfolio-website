db43fd0582b1c9e3ecf99e6001197d3e9c4b8af0
JackTracerSpore <jack.hawkins@mycelium.ventures>
2022-08-03 00:46:11 +0100

    updated config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
+	env: {
+		siteTitle: "Jacks Portfolio",
+		siteDescription:
+		  "My technology journey so far!",
+		siteUrl: "https://portfolio-website-gilt-pi.vercel.app/",
+		siteImagePreviewUrl: "preview image path in png format",
+	  },
 };
 
 module.exports = nextConfig;
