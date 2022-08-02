5e96d57c39b8b4958d64fb1123a02c9f25264cc2
JackTracerSpore <jack.hawkins@mycelium.ventures>
2022-08-02 13:59:21 +0100

    [wip] - adding skills

--- components/Skills/Skills.tsx
+++ components/Skills/Skills.tsx
@@ -2,16 +2,16 @@ import React from "react";
 
 const MySkills = () => {
 	return (
-		<div id="skills" className="relative flex h-screen w-screen p-20">
-			<div className="grid grid-cols-3 items-center gap-x-12 gap-y-4 p-20">
-				<div>
-					<img src="/static/img/me.png" />
+		<div id="skills" className="relative flex h-screen w-screen pt-40">
+			<div className="grid h-2/4 w-2/4 grid-cols-3 items-center gap-y-4 gap-x-12 pl-10 pr-10">
+				<div className="justify-center">
+					<img src="/static/img/skills/icons8-solidity-500.png" />
 				</div>
 				<div>
-					<img src="/static/img/me.png" />
+					<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original-wordmark.svg" />
 				</div>
 				<div>
-					<img src="/static/img/me.png" />
+					<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" />
 				</div>
 				<div>
 					<img src="/static/img/me.png" />
@@ -32,10 +32,8 @@ const MySkills = () => {
 					<img src="/static/img/me.png" />
 				</div>
 			</div>
-			<div>
-				<h1>Languages</h1>
-			</div>
-			<div className="grid grid-cols-3 items-center gap-x-12 gap-y-4 p-20">
+
+			<div className=" grid h-2/4 w-2/4 grid-cols-3 items-center gap-x-12 gap-y-4 pr-10 pl-10">
 				<div>
 					<img src="/static/img/me.png" />
 				</div>
