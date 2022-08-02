a6f98c7cbd471809e8e93b8a5ad3dc1036941d72
JackTracerSpore <jack.hawkins@mycelium.ventures>
2022-08-02 16:52:42 +0100

    Skills done

--- components/Skills/Skills.tsx
+++ components/Skills/Skills.tsx
@@ -3,72 +3,73 @@ import React from "react";
 const MySkills = () => {
 	return (
 		<div id="skills" className="relative flex h-screen w-screen pt-40">
-			<div className="grid h-2/4 w-2/4 grid-cols-3 items-center gap-y-4 gap-x-12 pl-10 pr-10">
-				<div className="justify-center">
-					<img src="/static/img/skills/icons8-solidity-500.png" />
-					<p className="text-center text-white">Solidity</p>
-				</div>
-				<div className="justify-center">
-					<img src="/static/img/skills/icons8-java-500.png" />
-					<p className="text-center text-white">Java</p>
-				</div>
-				<div className="justify-center">
-					<img src="/static/img/skills/icons8-python-500.png" />
-					<p className="text-center text-white">Python</p>
-				</div>
-				<div className="justify-center">
-					<img src="/static/img/skills/icons8-typescript-500.png" />
-					<p className="text-center text-white">Typescript</p>
-				</div>
-				<div className="justify-center">
-					<img src="/static/img/skills/icons8-javascript-logo-500.png" />
-					<p className="text-center text-white">Javascript</p>
-				</div>
-				<div className="justify-center">
-					<img src="/static/img/skills/icons8-c-sharp-logo-500.png" />
-					<p className="text-center text-white">c#</p>
-				</div>
-				<div className="justify-center">
-					<img src="/static/img/skills/icons8-html-5-500.png" />
-					<p className="text-center text-white">HTML-5</p>
-				</div>
-				<div className="justify-center">
-					<img src="/static/img/skills/icons8-css3-500.png" />
-					<p className="text-center text-white">CSS3</p>
-				</div>
-				<div className="justify-center">
-					<img src="/static/img/skills/icons8-sql-500.png" />
-					<p className="text-center text-white">SQL</p>
-				</div>
-			</div>
-
-			<div className=" grid h-2/4 w-2/4 grid-cols-3 items-center gap-x-12 gap-y-4 pr-10 pl-10">
-				<div>
-					<img src="/static/img/me.png" />
-				</div>
-				<div>
-					<img src="/static/img/me.png" />
-				</div>
-				<div>
-					<img src="/static/img/me.png" />
-				</div>
-				<div>
-					<img src="/static/img/me.png" />
-				</div>
-				<div>
-					<img src="/static/img/me.png" />
-				</div>
-				<div>
-					<img src="/static/img/me.png" />
-				</div>
-				<div>
-					<img src="/static/img/me.png" />
-				</div>
-				<div>
-					<img src="/static/img/me.png" />
-				</div>
-				<div>
-					<img src="/static/img/me.png" />
+			<div className="relative h-screen w-screen">
+				<h1 className="pb-20 text-center text-3xl text-mycelium-lightgreen">
+					Languages & Frameworks
+				</h1>
+				<div className=" flex w-screen justify-center">
+					<div className="grid h-3/4 w-2/4 grid-cols-5 items-center gap-y-14 gap-x-14 ">
+						<div>
+							<img src="/static/img/skills/icons8-solidity-500.png" />
+							<p className="pt-4 pt-4 text-center text-white">Solidity</p>
+						</div>
+						<div>
+							<img src="/static/img/skills/icons8-java-500.png" />
+							<p className="pt-4 pt-4 text-center text-white">Java</p>
+						</div>
+						<div>
+							<img src="/static/img/skills/icons8-python-500.png" />
+							<p className="pt-4 pt-4 text-center text-white">Python</p>
+						</div>
+						<div>
+							<img src="/static/img/skills/icons8-typescript-500.png" />
+							<p className="pt-4 pt-4 text-center text-white">Typescript</p>
+						</div>
+						<div>
+							<img src="/static/img/skills/icons8-javascript-logo-500.png" />
+							<p className="pt-4 pt-4 text-center text-white">Javascript</p>
+						</div>
+						<div>
+							<img src="/static/img/skills/icons8-c-sharp-logo-500.png" />
+							<p className="pt-4 pt-4 text-center text-white">c#</p>
+						</div>
+						<div>
+							<img src="/static/img/skills/icons8-html-5-500.png" />
+							<p className="pt-4 text-center text-white">HTML-5</p>
+						</div>
+						<div>
+							<img src="/static/img/skills/icons8-css3-500.png" />
+							<p className="pt-4 text-center text-white">CSS3</p>
+						</div>
+						<div>
+							<img src="/static/img/skills/icons8-sql-500.png" />
+							<p className="pt-4 text-center text-white">SQL</p>
+						</div>
+						<div>
+							<img src="/static/img/skills/icons8-react-500.png" />
+							<p className="pt-4 text-center text-white">React</p>
+						</div>
+						<div>
+							<img src="/static/img/skills/icons8-graphql-500.png" />
+							<p className="pt-4 text-center text-white">Graphql</p>
+						</div>
+						<div>
+							<img src="/static/img/skills/icons8-npm-500.png" />
+							<p className="pt-4 text-center text-white">NPM</p>
+						</div>
+						<div>
+							<img src="/static/img/skills/icons8-nodejs-500.png" />
+							<p className="pt-4 text-center text-white">Nodejs</p>
+						</div>
+						<div>
+							<img src="/static/img/skills/icons8-tailwindcss-500.png" />
+							<p className="pt-4 text-center text-white">Tailwindcss</p>
+						</div>
+						<div>
+							<img src="/static/img/skills/icons8-hardhat.png" />
+							<p className="pt-4 text-center text-white">Hardhat</p>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
