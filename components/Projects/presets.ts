bf5e220239f07afe2027ae097f45b0bb7b38f50f
JackTracerSpore <jack.hawkins@mycelium.ventures>
2022-08-03 00:25:27 +0100

    fixing build errors

---
+++ components/Projects/presets.ts
@@ -0,0 +1,29 @@
+export const projects: any = {
+	oart: [
+		{
+			name: "Mobile Phone Application",
+			description:
+				"An Android phone application for visitors to the EPIC wildlife and conservation project. EPICs mission statement is twofold: To create a small but significant wildlife reserve in an area primarily targeted for new housing, and to make the reserve accessible and of maximum interest to the public in order to promote physical and mental well-being and education.",
+			link: "https://github.com/JackG-eth/Dissertation/tree/master/Epic",
+			image: "/static/img/projects/epic.jpg",
+		},
+	],
+	pba: [
+		{
+			name: "Practical Business Analytics",
+			description:
+				"Analysing periodic Santander bike usage in London to predict future demand.",
+			link: "https://github.com/JackG-eth/Data_Science_Analytics_Coursework/tree/master/RubixOnRailsRApplication",
+			image: "/static/img/projects/pba.jpg",
+		},
+	],
+	speech: [
+		{
+			name: "Speech Recognition Using Transformers",
+			description:
+				"Exploring speech recognition technology to provide a hands-free method of communicating requests while in an elevator.",
+			link: "https://github.com/JackG-eth/Speech_Recognition_Using_Transformers",
+			image: "/static/img/projects/ai.jpg",
+		},
+	],
+};
